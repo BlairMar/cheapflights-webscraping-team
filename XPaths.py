4c6949455ebb035281cd573546d561c446f2afc0
tajpatel58 <tajpatel58@gmail.com>
2021-11-10 18:27:15 +0000

    Adding Column names and Xpaths for Hotel Info

--- XPaths.py
+++ XPaths.py
@@ -4,3 +4,20 @@ stays = '//a[@aria-label="Search for hotels"]'
 hotels_searchbox = '//div[@role="textbox"]//div[@class="lNCO-inner"]'
 search_button = '//button[@aria-label="Search"]'
 exit_datebox = '//h2[@class="x92x-header x92x-pres-header-default"]'
+hotel_name = '//h1[@class="c3xth-hotel-name"]'
+address = '//div[@class="c3xth-address"]'
+rating = '//span[@class="YlEV-rating-score"]'
+reviewer_count = '//div[@class="YlEV-review-count"]'
+cost = '//span[@class="c3xth-price"]'
+provider = '//span[@class="c3xth-provider"]'
+
+xpath_dict = {'Hotel Name': hotel_name,
+              'Hotel Address' : address,
+              'Average Rating' : rating,
+              'Number of Reviews': reviewer_count,
+              'Cost of Stay' : cost,
+              'Provider' : provider}
+
+
+column_names = ['Hotel Name', 'Hotel Address', 'Average Rating', 'Number of Reviews', 'Cost of Stay', 'Provider']
+info_dict = dict.fromkeys(column_names)
