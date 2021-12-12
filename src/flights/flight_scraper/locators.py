0aa0edd543c678f0ead18d3664e3731aba1c06c2
Daniel Lund <daniel.lund@bath.edu>
2021-12-12 20:54:08 +0000

    Removes all CSV files from previous scraper tests, changes xpaths to locate specific elements instead of the container

--- src/flights/flight_scraper/locators.py
+++ src/flights/flight_scraper/locators.py
@@ -2,5 +2,11 @@ LOCATORS_DICT = {
 'COOKIES_POPUP' : '//button[@title="Accept"]',
 'FLIGHTS_CARD' : '//div[@class="resultWrapper"]',
 'AIRLINE' : './/span[@class="codeshares-airline-names"]',
-
+'DEPART_TIMES': './/span[@class="depart-time base-time"]',
+'ARRIVAL_TIMES': './/span[@class="arrival-time base-time"]',
+'NUM_STOPS': './/span[@class="stops-text"]',
+'FLIGHT_TIMES': './/div[@class="section duration allow-multi-modal-icons"]//div[@class="top"]',
+'PRICE': './/span[@class="price-text"]',
+'AIRPORTS': './/div[@class="section times"]//div[@class="bottom"]',
+'LAYOVER': './/span[@class="js-layover"]'
 }
