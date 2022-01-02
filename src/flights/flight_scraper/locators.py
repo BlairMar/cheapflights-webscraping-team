30a099f41c11d8735cee9aeddd5b987c29f071ee
Daniel Lund <daniel.lund@bath.edu>
2022-01-02 15:36:19 +0000

    Creates threading class with bounded semaphore to limit how many threads are created/active, also creates new file to create schemas and tables for rds to move csv data to rds

--- src/flights/flight_scraper/locators.py
+++ src/flights/flight_scraper/locators.py
@@ -4,7 +4,7 @@ LOCATORS_DICT = {
 'AIRLINE' : './/span[@class="codeshares-airline-names"]',
 'DEPART_TIMES': './/span[@class="depart-time base-time"]',
 'ARRIVAL_TIMES': './/span[@class="arrival-time base-time"]',
-'NUM_STOPS': './/span[@class="stops-text"]',
+'NUM_STOPS': './/span[contains(@class, "stops-text")]',
 'FLIGHT_TIMES': './/div[@class="section duration allow-multi-modal-icons"]//div[@class="top"]',
 'PRICE': './/span[@class="price-text"]',
 'AIRPORTS': './/div[@class="section times"]//div[@class="bottom"]',
