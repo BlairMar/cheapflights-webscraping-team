348c34acc899e5157482f220721de20c0cb977d1
tajpatel58 <tajpatel58@gmail.com>
2022-01-21 13:08:26 +0000

    Updating XPaths

--- src/Hotels/XPaths.py
+++ src/Hotels/XPaths.py
@@ -8,16 +8,18 @@ hotel_box = '//div[@role="textbox"]'
 hotels_box = '//div[@role="textbox"]'
 search_button = '//button[@aria-label="Search"]'
 exit_datebox = '//h2[@class="x92x-header x92x-pres-header-default"]'
-hotel_name = '//h1[@class="c3xth-hotel-name"]'
+hotel_name_xpath = '//h1[@class="c3xth-hotel-name"]'
 address = '//div[@class="c3xth-address"]'
-rating = '//span[@class="YlEV-rating-score"]'
-reviewer_count = '//div[@class="YlEV-review-count"]'
+old_rating = '//span[@class="YlEV-rating-score"]'
+rating = '//div[@class="l3xK-reviews-summary-score"]'
+reviewer_count = '//div[@class="l3xK-reviews-summary-review-data"]'
+old_reviewer_count = '//div[@class="YlEV-review-count"]'
 cost = '//span[@class="c3xth-price"]'
 provider = '//span[@class="c3xth-provider"]'
 showmore = '//a[@class="moreButton"]'
 hotel_results = '//div[@class="FLpo"]'
 
-xpath_dict = {'Hotel Name': hotel_name,
+xpath_dict = {'Hotel Name': hotel_name_xpath,
               'Hotel Address' : address,
               'Average Rating' : rating,
               'Number of Reviews': reviewer_count,
@@ -27,6 +29,9 @@ xpath_dict = {'Hotel Name': hotel_name,
 
 column_names = ['City', 'Hotel Name', 'Hotel Address', 'Average Rating', 'Number of Reviews', 'Cost of Stay', 'Provider']
 info_dict = dict.fromkeys(column_names)
+
+cheap_flights_url = 'https://www.cheapflights.co.uk'
+images_xpath = '//div[@class="f800-image-container"]'
 
 cities = ['Alicante',
  'Amsterdam',
