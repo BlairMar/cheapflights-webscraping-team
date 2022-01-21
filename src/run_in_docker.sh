ff2afc52ee69cc1d0fc6cd800f2660aca0382be8
Daniel Lund <daniel.lund@bath.edu>
2022-01-21 17:51:11 +0000

    Finally gets chrome to install adds shell script to run python file and setup xvfb display

--- src/run_in_docker.sh
+++ src/run_in_docker.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-export DISPLAY=:20 && 
-Xvfb :20 -screen 0 1366x768x16 &&
-python3 flight_scraper/flight_scraper_main.py
+#! /bin/sh 
+
+export DISPLAY=:20
+Xvfb :20 -screen 0 1366x768x16 &
+python3 flights_script.py
 
