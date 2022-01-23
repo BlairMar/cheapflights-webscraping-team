4fcdb6d964dec032d5a42f7e2dff00e305a0be8c
tajpatel58 <tajpatel58@gmail.com>
2022-01-23 22:37:45 +0000

    Creating Methods to Scrape Hotel Photos

--- src/Hotels/Hotels_Scrape.py
+++ src/Hotels/Hotels_Scrape.py
@@ -130,14 +130,31 @@ class Hotel_Scraper:
         info['City'] = city_name
         for key in xpath_dict.keys():
             try:
-                info[key] = self.driver.find_element_by_xpath(xpath_dict[key]).text
+                info[key] = self.driver.find_element_by_xpath(xpath_dict[key]).text.replace('\n','')
             except:
                 continue
         return info 
 
+    
+    def get_hotel_photos(self, city_name, hotel_name):
+        images = self.driver.find_elements(By.XPATH, images_xpath)
+        attributes = [pic.get_attribute('style') for pic in images]
+        pattern = re.compile('\([^)]+\)')
+        url_ends = [url[2:-2] for url in [pattern.search(attribute)[0] for attribute in attributes]]
+        for idx, url in enumerate(url_ends):
+            image_url = cheap_flights_url + url
+            directory = f'./Cleaned_Data/{city_name}/Hotel_Pictures/{hotel_name}'
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
+            try:
+                urllib.request.urlretrieve(image_url, f'./Cleaned_Data/{city_name}/Hotel_Pictures/{hotel_name}/{hotel_name}_{idx+1}.jpg')
+            except:
+                pass 
 
 
-    def hotels_in_city_scraper(self, city_name, start_date, end_date, save=True):
+
+
+    def hotels_in_city_scraper(self, city_name, start_date, end_date, num_hotels=20, save=True, photos=True):
         """
         An instance of a driver will be created, the driver will load the cheapflights website, 
         click the cookies, enter the "Stays" page, edit the search parameters with the city_name
@@ -172,23 +189,31 @@ class Hotel_Scraper:
         except:
             pass
 
-        #sleep(5)
-        #self.url_date_changer(start_date,end_date, city_name)
-        sleep(30)
+        sleep(20)
 
         hotels_information = pd.DataFrame()
 
         hotels = self.driver.find_elements(By.XPATH, hotel_results)
         hotel_results_page = self.driver.window_handles[0]
-        for hotel in hotels[0:3]: 
+        for hotel in hotels[0:min(len(hotels), num_hotels)]: 
             hotel.click()
             tabs = self.driver.window_handles
-            sleep(8)
+            sleep(4)
             self.driver.switch_to.window(tabs[1-tabs.index(hotel_results_page)])
             hotel_info = self.hotel_page_scrape(city_name)
             hotel_name = self.driver.find_element_by_xpath(hotel_name_xpath).text
             hotels_information = hotels_information.append(hotel_info,ignore_index=True)
+            if photos:
+                self.get_hotel_photos(city_name, hotel_name)
+        
 
+            self.driver.close()
+            self.driver.switch_to.window(hotel_results_page)
+
+        self.driver.quit()
+
+
+        """
             # Get Hotel Pictures:
             images = self.driver.find_elements(By.XPATH, images_xpath)
             attributes = [pic.get_attribute('style') for pic in images]
@@ -200,61 +225,65 @@ class Hotel_Scraper:
                 if not os.path.isdir(directory):
                     os.makedirs(directory)
                 urllib.request.urlretrieve(image_url, f'./Cleaned_Data/{city_name}/Hotel_Pictures/{hotel_name}/{hotel_name}_{idx}.jpg')
-
-
-            self.driver.close()
-            self.driver.switch_to.window(hotel_results_page)
-
-
-
-
-
-        self.driver.quit()
+        """
 
         if save:
             hotels_information.to_csv(f'./Raw_Data/{city_name}.csv',index=False)
             cleaned_data = self.clean_data(hotels_information)
-            cleaned_data.to_csv(f'./Cleaned_Data/{city_name}/{city_name}.csv')
+            cleaned_data.to_csv(f'./Cleaned_Data/{city_name}/{city_name}.csv', index=False)
+            cleaned_data.to_csv(f'./Cleaned_CSVs/{city_name}.csv', index=False)
 
         return hotels_information
+
+    
+    def _number_finder(self, string):
+        pattern = r'[0-9]*,?[0-9]+'
+        value = re.compile(pattern)
+        try: 
+            return value.search(string)[0]
+        except:
+            return pd.NA
 
     def clean_data(self, df): 
         # Clean Data:
 
         #Below uses regular expressions to extract the cost of stay, an example uncleaned would be: £449total, cleaned would be: 449
-        pattern = r'[0-9]+,?[0-9]+'
-        value = re.compile(pattern)
-        df['Cost of Stay'] = df['Cost of Stay'].apply(lambda cost: value.search(cost)[0])
-        df['Number of Reviews'] = df['Number of Reviews'].apply(lambda review: value.search(review)[0])
+        df['Cost of Stay'] = df['Cost of Stay'].apply(lambda cost: self._number_finder(cost))
+        df['Number of Reviews'] = df['Number of Reviews'].apply(lambda review: self._number_finder(review))
         return df 
 
 
-    def scrape_all_info(self):
+    def scrape_all_info(self, start_date, end_date):
         """
-        Calling this function will scrape the top 10 hotels' information from all the popular cities,
+        Calling this function will scrape the top 20 hotels' information from all the popular cities,
         and store the information for each city within a CSV file in the "Hotels Information" directory. 
         """
-        for city in self.cities:
-            city_hotels = self.hotels_in_city_scraper(city)
-            city_hotels.to_csv(f'{os.getcwd()}/Hotels_Information/{self.city}.csv',index=False)
+
+        missing = ['Bali', 'Bangkok', 'Dalaman', 'Las Vegas', 'Murcia', 'Belfast']
+        for city in missing:
+            self.hotels_in_city_scraper(city, start_date, end_date, save=True)
 
 
 def thread(city_name, start_date, end_date):
     scraper = Hotel_Scraper()
-    scraper.hotels_in_city_scraper(city_name, start_date, end_date)
+    scraper.hotels_in_city_scraper(city_name, start_date, end_date, save=True)
 
 
 def scrape_data(start_date, end_date):
+    func = lambda city: thread(city, start_date, end_date, save=True)
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        func = lambda city: thread(city, start_date, end_date)
-        executor.map(func,cities[:2])
+        futures = [executor.submit(func, city) for city in cities]
+        #executor.map(func,cities)
 
 
 
 if __name__ == '__main__':
-    print(hotel_name_xpath)
-    scraper = Hotel_Scraper()
-    scraper.hotels_in_city_scraper('Barcelona','2022-02-12','2022-02-16',save=True)
 
+    scraper = Hotel_Scraper()
+    #scraper.hotels_in_city_scraper('Barcelona','2022-02-10','2022-02-14',save=True)
+    scraper.scrape_all_info('2022-02-10', '2022-02-14')
+
+
+# %%
 
 # %%
