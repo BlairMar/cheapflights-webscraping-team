84d28da039bc725a14b4e0e0d7c02d7ae5f25660
tajpatel58 <tajpatel58@gmail.com>
2022-01-24 20:54:04 +0000

    Changing S3 Upload Method from Client to Resource

--- src/Hotels/Data_Scraper_Script.py
+++ src/Hotels/Data_Scraper_Script.py
@@ -3,7 +3,6 @@ import pandas as pd
 import re 
 import boto3
 import os
-import sys
 
 city = input('Enter City that you would like Top hotels for: ')
 start_date = input('Enter Start date of Journey in the format (YYYY-MM-DD): ')
@@ -21,34 +20,24 @@ scraper = Hotel_Scraper()
 hotel_data = scraper.hotels_in_city_scraper(city, start_date, end_date, photos=photo_ind, num_hotels=int(number_of_hotels), save=True)
 hotel_data = scraper.clean_data(hotel_data)
 
+
+#%%
+import boto3
 # Add Connection to S3 Bucket, to store our data. 
-s3_bucket = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 def uploadDirectory(city,bucketname='cheapflights-scraper-hotel-data'):
+    s3_bucket = s3.Bucket(bucketname)
     for root,dirs,files in os.walk(f'./Cleaned_Data/{city}'):
         for file in files:
             file_type = file[-3:]
             if file_type == 'csv':
-                s3_bucket.upload_file(os.path.join(root,file),bucketname,f'{folder_name}/{file}')
+                s3_bucket.upload_file(os.path.join(root,file),f'{folder_name}/{file}')
             else:
                 hotel_name = file.split('.')[0].replace('_',' ')[:-2]
-                s3_bucket.upload_file(os.path.join(root,file),bucketname,f'{folder_name}/Hotel Pictures/{hotel_name}/{file}')
+                s3_bucket.upload_file(os.path.join(root,file),f'{folder_name}/Hotel Pictures/{hotel_name}/{file}')
 
+            
 uploadDirectory(city)
 
-#s3_bucket.upload_file(f'./Cleaned_Data/{city}', f'User_Hotels_Information/{folder_name}')
-"""
-import boto3
-folder_name='Florence_data'
-s3_bucket = boto3.client('s3')
-for root,dirs,files in os.walk(f'./Cleaned_Data/Florence'):
-    for file in files:
-        file_type = file[-3:]
-        if file_type == 'csv':
-            s3_bucket.upload_file(os.path.join(root,file),'cheapflights-scraper-hotel-data',f'{folder_name}/{file}')
-        else:
-            hotel_name = file.split('.')[0].replace('_',' ')[:-2]
-            s3_bucket.upload_file(os.path.join(root,file),'cheapflights-scraper-hotel-data',f'{folder_name}/Hotel Pictures/{hotel_name}/{file}')
-
-
-"""
+# %%
